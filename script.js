bc80f37fb6346edb67cc0e66a9ad4319b0949495
aminSimyari <149312884+aminSimyari@users.noreply.github.com>
2025-10-07 01:30:40 +0330

    added Fill skill bars

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-      const navLinks = document.querySelectorAll("nav a");
+  const navLinks = document.querySelectorAll("nav a");
   const currentPage = window.location.pathname.split("/").pop();
 
-
-  navLinks.forEach(link => {
+  navLinks.forEach((link) => {
     const linkPage = link.getAttribute("href");
     if (linkPage === currentPage) {
       link.classList.add("active");
@@ -11,3 +10,12 @@ document.addEventListener("DOMContentLoaded", function () {
       link.classList.remove("active");
     }
   });
+  const skillBars = document.querySelectorAll(".skill-progress");
+
+  if (skillBars.length > 0) {
+    skillBars.forEach((bar) => {
+      const value = bar.getAttribute("data-skill");
+      bar.style.width = value;
+    });
+  }
+});
